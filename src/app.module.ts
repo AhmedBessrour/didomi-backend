6a63d57ceaa15822d1899bbddda5e0ae23439d80
Ahmed Besrour <ahmed.bessrour@gmail.com>
2021-11-09 00:24:20 +0100

    :heavy_plus_sign: Add mongoDB integration
    adding config for mongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+
+import { databaseProviders } from 'src/common/providers/database.providers';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,6 +10,6 @@ import { EventsModule } from './events/events.module';
 @Module({
   imports: [UsersModule, EventsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
