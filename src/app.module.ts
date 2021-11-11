adfc77cdb16f546a5ba71f75bda144789a45f71e
Ahmed Besrour <ahmed.bessrour@gmail.com>
2021-11-11 14:45:38 +0100

    :fire: remove app controller
    remove unused app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
 import { UsersModule } from 'src/users/users.module';
 import { EventsModule } from 'src/events/events.module';
 
 @Module({
   imports: [UsersModule, EventsModule],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
